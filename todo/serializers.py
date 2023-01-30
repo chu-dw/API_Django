c6a014ba92157c231d23a8307f85a0c591470bb3
DongWon <ehddnjs1747@daum.net>
2023-01-30 17:34:03 +0900

    todo 등록 기능

--- todo/serializers.py
+++ todo/serializers.py
@@ -11,3 +11,9 @@ class TodoDetailSerializer(serializers.ModelSerializer):
     class Meta:
         model = Todo
         fields = ('id', 'title', 'description', 'created', 'complete', 'important')
+
+
+class TodoCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Todo
+        fields = ('title', 'description', 'important')
